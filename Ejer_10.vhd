c17c5100201a9ac48f22a14f291a54b0c0c45390
BryanAngamarca <105513572+BryanAngamarca@users.noreply.github.com>
2022-07-12 20:06:23 -0500

    Add files via upload

---
+++ Ejer_10.vhd
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   19:48:07 07/12/2022
+-- Design Name:   
+-- Module Name:   C:/Xilinx/Ejercicios02/Ejer_10.vhd
+-- Project Name:  Ejercicios02
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Ejer_10
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Ejer_10 IS
+END Ejer_10;
+ 
+ARCHITECTURE behavior OF Ejer_10 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Ejer_10
+    PORT(
+         clk : IN  std_logic;
+         C : IN  std_logic;
+         SAL1 : INOUT  std_logic;
+         SAL2 : INOUT  std_logic;
+         Q : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal C : std_logic := '0';
+
+	--BiDirs
+   signal SAL1 : std_logic;
+   signal SAL2 : std_logic;
+
+ 	--Outputs
+   signal Q : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Ejer_10 PORT MAP (
+          clk => clk,
+          C => C,
+          SAL1 => SAL1,
+          SAL2 => SAL2,
+          Q => Q
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      	wait for clk_period*20;
+		C <= '0';
+      wait for clk_period*20;
+		C <= '1';
+      wait;
+   end process;
+
+END;
---
+++ Ejer_6.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:10:45 07/12/2022 
+-- Design Name: 
+-- Module Name:    Ejer_6 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Ejer_6 is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           q : inout  STD_LOGIC_VECTOR (3 downto 0));
+end Ejer_6;
+
+architecture Behavioral of Ejer_6 is
+signal Qaux : STD_LOGIC_VECTOR (3 downto 0);
+begin
+process (clk,reset)
+begin
+	q<= Qaux;
+	if (clk' event and clk ='1') then
+		if(reset = '0' or Qaux ="101") then
+			Qaux <= "0000";
+		elsif(Qaux = "0000") then
+			Qaux <= Qaux+1;
+		else
+				Qaux <= Qaux+2;
+			end if;
+		end if;
+end process;
+end Behavioral;
+
